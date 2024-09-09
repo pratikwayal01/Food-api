5c998e96a3f15fa6eb0109192a76b56f0fa3adb1
Pratik Wayal <pratikswayl123@gamil.com>
2024-09-10 02:47:54 +0530

    not my code

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const fs = require('fs');
-
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'foodData.json'));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+server.listen(port);
